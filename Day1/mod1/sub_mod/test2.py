09ca19a7d3142971a57f4417bec77f8fcebd01f0
Knowledge Shelf <vishwajeetsinghrana8@gmail.com>
2021-09-13 16:20:57 +0530

    Add files via upload

---
+++ Day1/mod1/sub_mod/test2.py
@@ -0,0 +1,12 @@
+def func3(var1,var2):
+    var3 = var1 ** var2
+    
+    print("Pow:",var3)
+    
+def func4():
+    var1 = int(input("Enter the value of var1:"))
+    var2 = int(input("Enter the value of var2:"))
+    
+    var3 = var1 * var2
+    
+    return var3
