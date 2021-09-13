0463d09a449c0d5e8fee870229f39985ddd464a8
Knowledge Shelf <vishwajeetsinghrana8@gmail.com>
2021-09-13 16:23:10 +0530

    Add files via upload

---
+++ Day1/mod1/test.py
@@ -0,0 +1,12 @@
+def tabel():
+    num = int(input("Enter the value:"))
+    for i in range(1,11):
+        print(f"{num} * {i} = {num*i}")
+        
+        
+def func2():
+    var1 = int(input("Var1:"))
+    var2 = int(input("Var2:"))
+    
+    var3 = var1 + var2
+    print("Sum:",var3)
